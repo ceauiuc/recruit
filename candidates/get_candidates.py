570ac99954bff5d4803592d21bfeefc7bc618885
Simon Kim <skim104@illinois.edu>
2016-08-13 23:22:24 -0500

    renamed

---
+++ candidates/get_candidates.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+import os
+import sys
+
+
+semesters = ["14fa", "15sp", "15fa"]
+
+
+def main():
+    outdir = "results"
+    try:
+        os.makedirs(outdir)
+    except OSError:
+        if not os.path.exists(outdir):
+            print("check result directory")
+            sys.exit(1)
+
+    c = 6
+
+    lastnamefile_option = ""
+    for sem in semesters:
+        filename = "data/{}_deans.tsv".format(sem)
+        print("going through {} dean\'s list ...".format(sem))
+        command = "python3 find_koreans.py -i {} {} -o {} -c {}".format(filename, lastnamefile_option, outdir, c)
+        os.system(command)
+        print("  done")
+        lastnamefile_option = "-l {}/results-{}_deans/lastnames.txt".format(outdir, sem)
+
+if __name__ == "__main__":
+    main()
+
